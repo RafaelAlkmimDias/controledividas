3d911ff9767af35f1cc29ed928eb1027f4d762d3
Rafael <rafas07@hotmail.com>
2021-02-15 11:06:26 -0300

    criação das views

---
+++ resources/views/divida/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+
+@section('css')
+@endsection
+
+@section('content')
+    <h1> teste </h1>
+@endsection
+
+@section('js')
+@endsection
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,26 @@
+<?php date_default_timezone_set('America/Sao_Paulo'); ?>
+<!DOCTYPE html>
+
+<html dir="ltr" lang="pt">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Aplicação de exibição de controle de credito.">
+        <meta name="author" content="Rafael de Alkmim Dias">
+        <title>Controle de contas</title>
+        @yield('css')
+    </head>
+
+    <body>
+        
+        @yield('content')
+        
+        
+        @yield('js')
+    </body>
+
+
+
+</html>
